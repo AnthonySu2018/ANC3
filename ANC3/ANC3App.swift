5738a00d4eea58d2397ecf356e62784fcf21a893
anthony <anthonysu91@gmail.com>
2024-02-10 19:16:58 +0800

    Initial Commit

---
+++ ANC3/ANC3App.swift
@@ -0,0 +1,17 @@
+//
+//  ANC3App.swift
+//  ANC3
+//
+//  Created by AnthonySu on 2024/2/10.
+//
+
+import SwiftUI
+
+@main
+struct ANC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
